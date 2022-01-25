39c99cfbbb9644151a2afcef3566c1b843576750
Vitus Lindemann <vitus@adnovum.ch>
2022-01-25 15:01:16 +0100

    add person

--- python/src/runner/cluedo.py
+++ python/src/runner/cluedo.py
@@ -1,3 +1,21 @@
+from random import randint
+
+
+class Person:
+    def __init__(self, name, salutation, age, characteristic):
+        super(Person, self).__init__()
+        self.name = name
+        self.salutation = salutation
+        self.age = age
+        self.characteristic = characteristic
+
+    def print(self):
+        print("name: " + self.name
+              + ", salutation: " + self.salutation
+              + ", age: " + str(self.age)
+              + ", characteristic: " + self.characteristic)
+
 
 if __name__ == "__main__":
     print("hello world")
+
